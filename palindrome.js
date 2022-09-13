c0612b73ee60c092a9502339d9014f64a9fd8153
msumithra <112760072+msumithra@users.noreply.github.com>
2022-09-13 23:23:00 +0530

    git_practice_assignment

---
+++ palindrome.js
@@ -0,0 +1,9 @@
+let stvr="";
+for(let i=n-1;i>=0;i--){
+  stvr+=str[i];
+  
+}
+if(str==stvr){
+  console.log("Yes");
+}
+console.log("No");
---
+++ prime.js
@@ -0,0 +1,9 @@
+let num=23;
+let flag="No";
+for(let i=1;i<num;i++){
+  let(num%i===0){
+    flag="Yes";
+    break;
+  }
+}
+console.log(flag);
